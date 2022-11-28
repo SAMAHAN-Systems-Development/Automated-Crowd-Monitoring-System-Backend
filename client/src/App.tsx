6b475df6c2db556791c8ac730d694ff6a89f3d04
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-11-29 02:22:21 +0800

    Initial commit for client dir

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/styles.css';
+
+import {
+  Button
+} from './components/ComponentIndex';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
