ac55ff9fe7105a17ba76fe5e52e260486e5c5c73
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-11-28 22:44:48 +0800

    Formatted index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 import {
-    Authorize,
-    GetUsers,
-    GetUser,
-    UpdateEnteredStatus
-} from './utilities/UtilitiesIndex.js';
+  Authorize,
+  GetUsers,
+  GetUser,
+  UpdateEnteredStatus,
+} from "./utilities/UtilitiesIndex.js";
 
 const app = express();
-app.set('port', process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 
 // CORS
 app.use(cors());
@@ -20,79 +20,74 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // TODO: UPDATE WELCOME PAGE
-app.get('/', async (req, res) => {
-    try {
-        // res.send("Hello, world!")
-        res.status(200).json({"msg": "Hello, World!"});
-    }
-    catch (error) {
-        res.status(400).json({"msg": error})
-    }
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ msg: "Hello, World!" });
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
 });
 
-app.get('/api/users', async (req, res) => {
-    try {
-        const auth = await Authorize();
-        const users = await GetUsers(auth);
+app.get("/api/users", async (req, res) => {
+  try {
+    const auth = await Authorize();
+    const users = await GetUsers(auth);
 
-        res.status(200).json(users)
-    }
-    catch (error) {
-        res.status(400).json({"msg": error})
-    }
-})
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+});
 
 // Returns row as an object if it matches an id from the sheets
-app.get('/api/users/:id', async (req, res) => {
-    try {
-        const auth = await Authorize();
-        const result = await GetUser(auth, req.params.id);
+app.get("/api/users/:id", async (req, res) => {
+  try {
+    const auth = await Authorize();
+    const result = await GetUser(auth, req.params.id);
 
-        // IF USER IS NOT FOUND
-        if (result === null) {
-            throw "UserNotFound";
-        }
-
-        res.status(200).json(result);
+    // IF USER IS NOT FOUND
+    if (result === null) {
+      throw "UserNotFound";
     }
-    catch (error) {
-        if (error === "UserNotFound") {
-            res.status(404).json({msg: error});
-            return;
-        }
 
-        res.status(400).json({"msg": error})
+    res.status(200).json(result);
+  } catch (error) {
+    if (error === "UserNotFound") {
+      res.status(404).json({ msg: error });
+      return;
     }
+
+    res.status(400).json({ msg: error });
+  }
 });
 
 // Updates entered status if it matches an id from the sheets
-app.put('/api/users/:id', async (req, res) => {
-    const { entered } = req.body;
-    try {
-        const auth = await Authorize();
+app.put("/api/users/:id", async (req, res) => {
+  const { entered } = req.body;
+  try {
+    const auth = await Authorize();
 
-        const result = await UpdateEnteredStatus(auth, req.params.id, entered);
+    const result = await UpdateEnteredStatus(auth, req.params.id, entered);
 
-        // IF USER IS NOT FOUND
-        if (result === null) {
-            throw "UserNotFound";
-        }
-
-        res.status(200).json({
-            "msg": "Success!",
-            "result": result
-        })
+    // IF USER IS NOT FOUND
+    if (result === null) {
+      throw "UserNotFound";
     }
-    catch (error) {
-        if (error === "UserNotFound") {
-            res.status(404).json({msg: error});
-            return;
-        }
 
-        res.status(400).json({"msg": error})
+    res.status(200).json({
+      msg: "Success!",
+      result: result,
+    });
+  } catch (error) {
+    if (error === "UserNotFound") {
+      res.status(404).json({ msg: error });
+      return;
     }
-})
 
-app.listen(app.get('port'), () => {
-    console.log("SERVER ON PORT", app.get('port'));
+    res.status(400).json({ msg: error });
+  }
+});
+
+app.listen(app.get("port"), () => {
+  console.log("SERVER ON PORT", app.get("port"));
 });
